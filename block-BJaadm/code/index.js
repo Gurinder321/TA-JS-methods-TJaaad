1c34114a4fdffc3ebae6cdbe6a9f671d23d2bb22
Gurinder Khabra <gurinderkhabra@gmail.com>
2021-11-17 12:53:21 -0800

    Completed

--- block-BJaadm/code/index.js
+++ block-BJaadm/code/index.js
@@ -1,19 +1,22 @@
 let persons = [
-  { name: 'John', grade: 8, sex: 'M' },
-  { name: 'Sarah', grade: 12, sex: 'F' },
-  { name: 'Bob', grade: 16, sex: 'M' },
-  { name: 'Johnny', grade: 2, sex: 'M' },
-  { name: 'Ethan', grade: 4, sex: 'M' },
-  { name: 'Paula', grade: 18, sex: 'F' },
-  { name: 'Donald', grade: 5, sex: 'M' },
-  { name: 'Jennifer', grade: 13, sex: 'F' },
-  { name: 'Courtney', grade: 15, sex: 'F' },
-  { name: 'Jane', grade: 9, sex: 'F' },
-  { name: 'John', grade: 17, sex: 'M' },
-  { name: 'Arya', grade: 14, sex: 'F' },
+  { name: "John", grade: 8, sex: "M" },
+  { name: "Sarah", grade: 12, sex: "F" },
+  { name: "Bob", grade: 16, sex: "M" },
+  { name: "Johnny", grade: 2, sex: "M" },
+  { name: "Ethan", grade: 4, sex: "M" },
+  { name: "Paula", grade: 18, sex: "F" },
+  { name: "Donald", grade: 5, sex: "M" },
+  { name: "Jennifer", grade: 13, sex: "F" },
+  { name: "Courtney", grade: 15, sex: "F" },
+  { name: "Jane", grade: 9, sex: "F" },
+  { name: "John", grade: 17, sex: "M" },
+  { name: "Arya", grade: 14, sex: "F" },
 ];
 
 // Create an array peopleName and store value of sex key from persons array
+
+let peopleName = [];
+console.log(persons.sex);
 
 // Create an array peopleGrade and store the value of grade key from persons array
 
@@ -21,17 +24,51 @@ let persons = [
 
 // Log the filtered named of people in peopleName that starts with 'J' or 'P'
 
-// Log the length of filtered named of people in peopleName that starts with 'A' and 'C'
+let filtersPersonsJ = persons.filter((person) => person.name.startsWith("J"));
+console.log(filtersPersonsJ);
+let filtersPersonsP = persons.filter((person) => person.name.startsWith("P"));
+console.log(filtersPersonsP, filtersPersonsJ);
+
+let merged = [...filtersPersonsJ, ...filtersPersonsP];
+console.log(merged);
+
+// Log the length of filtered names of people in peopleName that starts with 'A' and 'C'
+
+let filtersPersonsA = persons.filter((person) => person.name.startsWith("A"));
+console.log(filtersPersonsA);
+let filtersPersonsC = persons.filter((person) => person.name.startsWith("C"));
+console.log(filtersPersonsC);
+
+let merged1 = [...filtersPersonsA, ...filtersPersonsC];
+console.log(merged1);
 
 // Log all the filtered male ('M') in persons array
 
+let filtersPersonsSex = persons.filter((person) => person.sex.startsWith("M"));
+console.log(filtersPersonsSex);
+
 // Log all the filtered female ('F') in persons array
+
+let filtersPersonsSexi = persons.filter((person) => person.sex.startsWith("F"));
+console.log(filtersPersonsSexi);
 
 // Log all the filtered female ('F') whose name starts with 'C' or 'J' in persons array
 
+console.log(filtersPersonsJ);
+console.log(filtersPersonsC);
+
+let merged11 = [...filtersPersonsJ, ...filtersPersonsC];
+console.log(merged11);
+
+let filtersPersonsSexi1 = merged11.filter((person) => person.sex.startsWith("F"));
+console.log(filtersPersonsSexi1);
+
 // Log all the even numbers from peopleGrade array
+let evenPersonGrade = persons.filter((person) => person.grade % 2 == 0);
+console.log(evenPersonGrade);
 
 // Find the first name that starts with 'J' in persons array and log the object
+// let filtersPersonsJ = persons.filter((person) => person.name.startsWith("J"));
 
 // Find the first name that starts with 'P' in persons array and log the object
 
